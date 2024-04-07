5b8c4b3668bf2e0ff1f7a6742899f2cfa64bf40f
Rafal <rafalgad@wp.pl>
2024-04-07 14:03:17 +0200

    zad1

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.awt.Point;
+
+public class Main {
+    public static void main(String[] args) {
+        Point punkt = new Point(7,2);
+        System.out.println("Współrzędne punktu: (" + punkt.getX() + ", " + punkt.getY() + ")");
+    }
+}
