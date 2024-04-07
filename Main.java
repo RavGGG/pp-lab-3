4f52e0ef5ddd30fe7297198bd6a5e93c9e844e1d
Rafal <rafalgad@wp.pl>
2024-04-07 14:22:23 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
-import java.awt.Point;
+import geometry.Circle;
+import geometry.Point;
 
 public class Main {
     public static void main(String[] args) {
-        Point punkt = new Point(7,2);
-        System.out.println("Współrzędne punktu: (" + punkt.getX() + ", " + punkt.getY() + ")");
+        Circle circle = new Circle(new Point(7, 2), 6);
+
+        System.out.println("Środek koła: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
+        System.out.println("Promień koła: " + circle.getRadius());
+        System.out.println("Obwód koła: " + circle.calculatePerimeter());
+        System.out.println("Pole powierzchni koła: " + circle.getArea());
     }
 }
